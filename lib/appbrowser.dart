d5f68e25143482042b895802d772164398e99f6c
Augustine Addey <aaddey@payswitch.com.gh>
2021-03-10 08:55:32 +0000

    dependency fix

--- lib/appbrowser.dart
+++ lib/appbrowser.dart
@@ -2,32 +2,17 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class CheckoutAppBrowser extends InAppBrowser {
   @override
-  Future onLoadStart(String url) async {
+  Future onLoadStart(url) async {
     print("\n\nStarted $url\n\n");
-    // print(
-    //     'Checkout url match :: ${watch.toString().contains(Uri.dataFromString(url).host)}');
   }
 
   @override
-  Future onLoadStop(String url) async {
+  Future onLoadStop(url) async {
     print("\n\nStopped $url\n\n");
-    // print(
-    //     'Checkout url match :: ${watch.toString().contains(Uri.dataFromString(url).host)}');
-    // if (watch.toString().contains(Uri.dataFromString(url).host)) {
-    //   Uri currentUri = Uri(host: url);
-    //   Map<String, dynamic> data = {
-    //     "code": currentUri.queryParameters['code'],
-    //     "status": currentUri.queryParameters['status'],
-    //     "reason": currentUri.queryParameters['reason'],
-    //     "transaction_id": currentUri.queryParameters['transaction_id']
-    //   };
-
-    //   this.onLoadFinish(data);
-    // }
   }
 
   @override
-  void onLoadError(String url, int code, String message) {
+  void onLoadError(url, code, message) {
     print("\n\nCan't load $url.. Error: $message\n\n");
   }
 
